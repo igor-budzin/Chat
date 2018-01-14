91c0e444ef90c72b4ff705d773d7758c4aaa5804
Igor Budzinsky <igor.budzin95@gmail.com>
2018-01-14 23:59:23 +0200

    thunk

--- client/src/reducers/messageReducer.js
+++ client/src/reducers/messageReducer.js
@@ -1,12 +1,15 @@
 import {SEND_MESSAGE} from '../configs/actionConstans';
 
 const initialState = {
-
+    loaded: false,
+    message: 'Just created',
+    connected: false
 }
 
 export default function messageReducer(state = initialState, action) {
     switch(action.type) {
         case SEND_MESSAGE:
+        console.log("SEND_MESSAGE");
             return state;
 
         default:
--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,10 +1,13 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
 
 export default function configureStore(initialState) {
     const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-    const store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk), devtools));
+    const logger = createLogger()
+
+    const store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk, logger), devtools));
 
     if (module.hot) {
         module.hot.accept('../reducers/rootReducer', () => {
