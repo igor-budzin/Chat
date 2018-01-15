af60a9cc166e00600069c5442d85cb9eb1d34678
Igor <igor.budzin95@gmail.com>
2018-01-15 10:56:23 +0200

    socket api

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,13 +1,15 @@
 import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
 import {createLogger} from 'redux-logger';
+import socketMiddleware from '../middlewares/socketMiddleware';
 import rootReducer from '../reducers/rootReducer';
 
 export default function configureStore(initialState) {
     const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-    const logger = createLogger()
+    const logger = createLogger();
 
-    const store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk, logger), devtools));
+    const middlewares = [socketMiddleware(), logger];
+
+    const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middlewares), devtools));
 
     if (module.hot) {
         module.hot.accept('../reducers/rootReducer', () => {
